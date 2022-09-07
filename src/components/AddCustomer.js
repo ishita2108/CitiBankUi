01fdb0f774f73c85e9e8f3a75b44bbace39c1af1
Ishita Arora <iwinner21@gmail.com>
2022-09-07 09:50:05 +0530

    Validations

--- src/components/AddCustomer.js
+++ src/components/AddCustomer.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import CustomerService from '../services/CustomerService';
 //import Moment from 'moment';
@@ -20,29 +20,60 @@ const AddCustomer = () => {
         passportNo:"",
         kycNo:""
     })
-    //const [formErrors, setFormErrors] = useState({});
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
 
-    const saveCustomer =(e) =>{
-        e.preventDefault();
-        CustomerService.saveCustomer(customer).then((response)=>{
-            navigate("/customerList")
-        }).catch((error)=>{
-            console.log(error)
-        })
-        
-    }
+    useEffect(() => {
+        console.log(formErrors)
+        if(Object.keys(formErrors).length === 0 && isSubmit){
+            console.log(customer)
+            CustomerService.saveCustomer(customer).then((response)=>{
+                navigate("/customerList")
+            }).catch((error)=>{
+                console.log(error)
+            })
+        }
+    }, [formErrors])
+    
     const handleSubmit = (e) => {
         e.preventDefault();
-        saveCustomer(e);
-      
-        //setFormErrors(validate(customer));
+        setFormErrors(validate(customer));
+        setIsSubmit(true);
     }
 
-    // const validate = (values) =>{
-    //     const errors = {}
-    //     const regex ="[A-Za-z]+( [A-Za-z]+)*"
-    //     return errors;
-    // }
+    const validate = (values) =>{
+        const errors = {}
+        const nameValid =/^[a-zA-Z ]*$/
+        const contactValid = /[789][0-9]{9}/
+        const panValid = /[A-Z]{5}[0-9]{4}[A-Z]{1}/
+        const aadharValid =/^[2-9]{1}[0-9]{3}\s{1}[0-9]{4}\s{1}[0-9]{4}$/
+        const passportValid = /^[A-PR-WY][1-9]\d\s?\d{4}[1-9]$/
+        const kycValid = /[1-9][0-9]{13}/
+        if(!nameValid.test(customer.name)){
+            errors.name = "Add proper name"
+        }
+
+        if(!contactValid.test(customer.contactDetails)){
+            errors.contactDetails = " Add proper Contact Number of 10 digits"
+        }
+
+        if(!panValid.test(customer.panNo)){
+            errors.panNo = " Add proper Pan Number"
+        }
+
+        if(!aadharValid.test(customer.aadharNo)){
+            errors.aadharNo = " Add proper Aadhar Number of 12 digits with spaces"
+        }
+
+        if(!passportValid.test(customer.passportNo)){
+            errors.passportNo = " Add proper Passport Number"
+        }
+
+        if(!kycValid.test(customer.kycNo)){
+            errors.kycNo = " Add proper KYC Number of 14  digits"
+        }
+        return errors;
+    }
        
 
     const handleChange =(e)=>{
@@ -86,67 +117,93 @@ const AddCustomer = () => {
                     onChange={(e) => handleChange(e)} required
                    className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
-
+            <p className='text-red-500'>{formErrors.name}</p>
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> Permanent Address</label>
                 <input type="text" name="permanentAddress"  value={customer.permanentAddress}
                     onChange={(e) => handleChange(e)} required
                     className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+            <p className='text-red-500'>{formErrors.permanentAddress}</p>
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> Current Address</label>
                 <input type="text" name="currentAddress" value={customer.currentAddress}
                     onChange={(e) => handleChange(e)} required
                     className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+
+            <p className='text-red-500'>{formErrors.currentAddress}</p>
+
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> Contact Details</label>
                 <input type="text" name="contactDetails" value={customer.contactDetails}
                     onChange={(e) => handleChange(e)} required
                     className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+
+            <p className='text-red-500'>{formErrors.contactDetails}</p>
+
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> Email</label>
                 <input type="email" name="email" value={customer.email}
                     onChange={(e) => handleChange(e)} required
                     className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+            <p className='text-red-500'>{formErrors.email}</p>
+
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> DOB</label>
                 <input type="date" format="yyyy-MM-dd" name="dob" value={customer.dob}
                     onChange={(e) => handleChange(e)} required
                     className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+
+            <p className='text-red-500'>{formErrors.dob}</p>
+
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> Pan Number</label>
                 <input type="text" name="panNo" value={customer.panNo}
                     onChange={(e) => handleChange(e)} required
                    className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+
+            <p className='text-red-500'>{formErrors.panNo}</p>
+
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> Aadhar Number</label>
                 <input type="text" name="aadharNo" value={customer.aadharNo}
                     onChange={(e) => handleChange(e)} required
                    className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+
+            <p className='text-red-500'>{formErrors.aadharNo}</p>
+
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> Passport Number</label>
                 <input type="text" name="passportNo" value={customer.passportNo}
                     onChange={(e) => handleChange(e)} required
                    className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+
+            <p className='text-red-500'>{formErrors.passportNo}</p>
+
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> KYC Number</label>
                 <input type="text" name="kycNo" value={customer.kycNo}
                     onChange={(e) => handleChange(e)} required
                    className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+
+            <p className='text-red-500'>{formErrors.kycNo}</p>
+
             <div className='items-center justify-center h-14 w-full my-4'>
                 <label className='block text-gray-600 text-sm  font-normal'> Occupation</label>
                 <input type="text" name="occupation" value={customer.occupation}
                     onChange={(e) => handleChange(e)}  required
                    className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
+
+            <p className='text-red-500'>{formErrors.occupation}</p>
 
             <div className='items-center justify-center h-14 w-full my-4 space-x-4 pt-4'>
                <button className='rounded text-white font-semibold bg-green-400 py-2 px-2 hover:bg-green-700' type='submit'>Save</button>
